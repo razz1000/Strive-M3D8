e00011a1d3ae9b048abe972f9961c4b73a7d3f2a
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-04-13 13:48:02 +0100

    Backend working and giving a 200 response

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert("welcome");
+let formOnSubmitFunction = async (event) => {
+  console.log(event.target);
+  event.preventDefault();
+
+  const theCurrentFormEvent = {
+    name: document.getElementById("name").value,
+    description: document.getElementById("description").value,
+    brand: document.getElementById("brand").value,
+    imageUrl: document.getElementById("image").value,
+    price: document.getElementById("price").value,
+  };
+
+  const response = await fetch(
+    "https://striveschool-api.herokuapp.com/api/product/",
+    {
+      method: "POST",
+      body: JSON.stringify(theCurrentFormEvent),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmNiZmE5MDIzOTAwMTVkOTY1YzYiLCJpYXQiOjE2NDk4NTE1ODMsImV4cCI6MTY1MTA2MTE4M30.1jc7LN9eMX4yZ7HWl4wazVJ2SyiKUzAHtFm0IglDnhY",
+      },
+    }
+  );
+
+  if (response.ok) {
+    const body = await response.json();
+
+    alert("the new server generated ID is:" + body._id);
+    console.log(response);
+  }
+};
